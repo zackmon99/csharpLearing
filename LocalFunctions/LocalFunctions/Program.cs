e51ea6b95b982634b675f00827960f289f8c12f9
zackmon99 <monzack4@gmail.com>
2020-07-03 17:08:13 -0700

    Local Functions

---
+++ LocalFunctions/LocalFunctions/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace LocalFunctions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10, b = 5;
+            int sum = Sum(a, b);
+            int difference = Difference(a, b);
+            Console.WriteLine($"The Sum of {a} and {b} is {sum}");
+            Console.WriteLine($"The Difference of {a} and {b} is {difference}");
+            int Sum(int x, int y)
+            {
+                return x + y;
+            }
+            int Difference(int x, int y)
+            {
+                return x - y;
+            }
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
