5fe6af2e8fb19fdb9d5cc06288762111a41be6a4
zackmon99 <monzack4@gmail.com>
2020-07-05 18:54:11 -0700

    Delegates and Custom exceptions.

---
+++ CustomExceptions/CustomExceptions/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+
+namespace CustomExceptions
+{
+    public class UserNotLoggedInException : Exception
+    {
+        public UserNotLoggedInException(string message) : base(message)
+        {
+        }
+
+        public UserNotLoggedInException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+
+    
+    public class OddNumberException : Exception
+    {
+        public override string Message
+        {
+            get
+            {
+                return "Divisor cannot be an odd number.";
+            }
+        }
+    }
+    public class User
+    {
+        private bool _loggedIn;
+
+        public bool LoggedIn
+        {
+            get
+            {
+                return _loggedIn;
+            }
+
+            set
+            {
+                _loggedIn = value;
+            }
+        }
+
+        public bool Logout()
+        {
+            Console.WriteLine("Logging out");
+            if (_loggedIn)
+            {
+                Console.WriteLine("User Logged out");
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int x, y, z;
+
+            Console.WriteLine("Is user logged in?");
+            bool loggedIn = bool.Parse(Console.ReadLine());
+
+            User user = new User();
+
+            if (loggedIn)
+            {
+                user.LoggedIn = true;
+            }
+            else
+            {
+                user.LoggedIn = false;
+            }
+
+            Console.WriteLine("Enter two integers");
+            x = int.Parse(Console.ReadLine());
+            y = int.Parse(Console.ReadLine());
+
+            try
+            {
+                if (y % 2 > 0)
+                {
+                    throw new OddNumberException();
+                }
+
+                z = x / y;
+                Console.WriteLine(z);
+            }
+            catch (OddNumberException one)
+            {
+                Console.WriteLine(one.Message);
+                Console.WriteLine("Logging out user");
+                
+                try
+                {
+                    if (!user.LoggedIn)
+                    {
+                        throw new UserNotLoggedInException("User not logged in", one);
+                    }
+                }
+                catch (UserNotLoggedInException unlie)
+                {
+                    Console.WriteLine(unlie.Message + " during \'" + unlie.InnerException.Message + "\' exception");
+                }
+            }
+
+            
+
+            Console.WriteLine("End of program");
+        }
+    }
+}
---
+++ Delegates/Delegates/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Dynamic;
+
+namespace Delegates
+{
+    public delegate void AddDelegate(int a, int b);
+    public delegate string GreetingsDelagate(string name);
+    public class Program
+    {
+        //NonStatic method
+        public void Add(int x, int y)
+        {
+            Console.WriteLine(@"The Sum of {0} and {1}, is {2} ", x, y, (x + y));
+        }
+        //Static Method
+        public static string Greetings(string name)
+        {
+            return "Hello @" + name;
+        }
+        static void Main(string[] args)
+        {
+            Program obj = new Program();
+            //calling non static method through object
+            // obj.Add(100, 100);
+            AddDelegate ad = new AddDelegate(obj.Add);
+
+            ad(100, 100);
+
+            GreetingsDelagate gd = new GreetingsDelagate(Program.Greetings);
+            //Calling static method with class name
+            string GreetingsMessage = gd("Zack");
+            Console.WriteLine(GreetingsMessage);
+            Console.ReadKey();
+        }
+    }
+}
